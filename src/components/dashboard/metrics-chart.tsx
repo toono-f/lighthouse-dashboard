ca620243908e6db3199e863749ab8ace510043e3
toono-f <52028799+toono-f@users.noreply.github.com>
2025-01-19 11:03:23 +0900

    MetricsChartコンポーネントに日本時間表示のTODOコメントを追加

--- src/components/dashboard/metrics-chart.tsx
+++ src/components/dashboard/metrics-chart.tsx
@@ -35,6 +35,8 @@ type DatasetVisibility = {
   [key: string]: boolean;
 };
 
+// TODO: 日付・時刻が日本時間で表示されるようにしたい
+
 export const MetricsChart = ({ data }: MetricsChartProps) => {
   // データセットの表示状態を管理するuseState
   const [visibleDatasets, setVisibleDatasets] = useState<DatasetVisibility>({
